fb3dbeddf999c3e2db4ba9547fd0e9e45a30a30f
Kadir Yilmaz <kadiryilmaz@MacBook-Pro.local>
2020-11-20 23:25:51 +0100

    Index controller and view created

--- ToDoList/Controllers/ToDoController.cs
+++ ToDoList/Controllers/ToDoController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ToDoList.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,10 +12,22 @@ namespace ToDoList.Controllers
 {
     public class ToDoController : Controller
     {
-        // GET: /<controller>/
-        public string Index()
+        // Database dependency
+        private readonly ToDoContext context;
+
+        public ToDoController(ToDoContext context)
         {
-            return "test";
+            this.context = context;
+        }
+        // GET: /<controller>/
+        public async Task<ActionResult> Index()
+        {
+            IQueryable<TodoList> items = from i in context.ToDoList orderby i.Id select i;
+
+            List<TodoList> todoList = await items.ToListAsync();
+
+            return View(todoList);
+
         }
     }
 }
